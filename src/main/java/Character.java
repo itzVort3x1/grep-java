587cf1d2c41a1970e5e685249948756a48c4c228
itzVort3x1 <saikaustubh10@gmail.com>
2025-02-01 13:13:05 +0000

    codecrafters submit [skip ci]

--- src/main/java/Character.java
+++ src/main/java/Character.java
@@ -1,11 +1,15 @@
 public class Character {
     private static String sourceStr = "";
+
     Character(String source) { this.sourceStr = source; }
+
     public String getSource() { return sourceStr; }
-    public static boolean isDigit(char c) {
+
+    private static boolean isDigit(char c) {
         return c >= '0' && c <= '9';
     }
-    public boolean checkDigit() {
+
+    private boolean checkDigit() {
         for (int i = 0; i < sourceStr.length(); i++) {
             if (Character.isDigit(sourceStr.charAt(i))) {
                 return true;
@@ -13,9 +17,26 @@ public class Character {
         }
         return false;
     }
+
+    private static boolean isLetter(char c) {
+        return c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z';
+    }
+
+    private boolean checkLetter() {
+        for(int i = 0; i < sourceStr.length(); i++) {
+            if(Character.isLetter(sourceStr.charAt(i))) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     public boolean matchPattern(String inputLine, String pattern) {
-        if (checkDigit()) {
-            return true;
+        if (pattern.equals("\\d")) {
+            return checkDigit();
+        } else if (pattern.equals("\\w")) {
+            return checkLetter();
         } else if (pattern.length() == 1) {
             return inputLine.contains(pattern);
         } else {
